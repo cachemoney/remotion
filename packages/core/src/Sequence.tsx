5d2dcb196b46f71afcaf34997af8fdb6e6991741
Robin <cachebroker@gmail.com>
2023-02-25 12:22:05 -0600

    merged

--- packages/core/src/Sequence.tsx
+++ packages/core/src/Sequence.tsx
@@ -6,13 +6,16 @@ import React, {
 	useMemo,
 	useState,
 } from 'react';
-import {AbsoluteFill} from './AbsoluteFill';
-import {CompositionManager} from './CompositionManager';
-import {useRemotionEnvironment} from './get-environment';
-import {getTimelineClipName} from './get-timeline-clip-name';
-import {useNonce} from './nonce';
-import {TimelineContext, useTimelinePosition} from './timeline-position-state';
-import {useVideoConfig} from './use-video-config';
+import {AbsoluteFill} from './AbsoluteFill.js';
+import {CompositionManager} from './CompositionManager.js';
+import {useRemotionEnvironment} from './get-environment.js';
+import {getTimelineClipName} from './get-timeline-clip-name.js';
+import {useNonce} from './nonce.js';
+import {
+	TimelineContext,
+	useTimelinePosition,
+} from './timeline-position-state.js';
+import {useVideoConfig} from './use-video-config.js';
 
 export type SequenceContextType = {
 	cumulatedFrom: number;
@@ -31,6 +34,7 @@ export type LayoutAndStyle =
 	| {
 			layout?: 'absolute-fill';
 			style?: React.CSSProperties;
+			className?: string;
 	  };
 
 export type SequenceProps = {
@@ -203,19 +207,23 @@ const SequenceRefForwardingFunction: React.ForwardRefRenderFunction<
 
 	return (
 		<SequenceContext.Provider value={contextValue}>
-			{content === null ? null : layout === 'absolute-fill' ? (
-				<AbsoluteFill ref={ref} style={defaultStyle}>
+			{content === null ? null : other.layout === 'none' ? (
+				content
+			) : (
+				<AbsoluteFill
+					ref={ref}
+					style={defaultStyle}
+					className={other.className}
+				>
 					{content}
 				</AbsoluteFill>
-			) : (
-				content
 			)}
 		</SequenceContext.Provider>
 	);
 };
 
 /**
- * A component that time-shifts its children and wraps them in an absolutely positioned <div>.
- * @link https://www.remotion.dev/docs/sequence
+ * @description A component that time-shifts its children and wraps them in an absolutely positioned <div>.
+ * @see [Documentation](https://www.remotion.dev/docs/sequence]
  */
 export const Sequence = forwardRef(SequenceRefForwardingFunction);
